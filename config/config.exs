eca4806e8ba2cf20ec601f1b08e86f3cd55f0809
twozeronine <amdusias209@gmail.com>
2022-08-26 12:11:11 +0900

    feat : application에 ecto 적용

---
+++ config/config.exs
@@ -0,0 +1,10 @@
+import Config
+
+config :music_db, ecto_repos: [MusicDB.Repo]
+
+config :music_db, MusicDB.Repo,
+  database: "music_db_repo",
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  port: 5432
--- lib/music_db/application.ex
+++ lib/music_db/application.ex
@@ -8,8 +8,7 @@ defmodule MusicDb.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: MusicDb.Worker.start_link(arg)
-      # {MusicDb.Worker, arg}
+      MusicDB.Repo
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
