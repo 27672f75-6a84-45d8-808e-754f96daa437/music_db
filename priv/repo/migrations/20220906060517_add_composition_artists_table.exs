1394e4f0fd85f69a2cee32b261501def1d32c519
twozeronine <amdusias209@gmail.com>
2022-09-06 15:40:21 +0900

    feat : compositions 과 artists를 many-to-many로 연결하고 기존에 compositions에서 artist_id 컬럼을 지움.

--- priv/repo/migrations/20220906060517_add_composition_artists_table.exs
+++ priv/repo/migrations/20220906060517_add_composition_artists_table.exs
@@ -1,5 +1,7 @@
 defmodule MusicDB.Repo.Migrations.AddCompositionArtistsTable do
   use Ecto.Migration
+  import Ecto.Query
+  alias MusicDB.Repo
 
   @table "compositions_artists"
   def change do
@@ -11,5 +13,23 @@ defmodule MusicDB.Repo.Migrations.AddCompositionArtistsTable do
 
     create index("compositions_artists", :composition_id)
     create index("compositions_artists", :artist_id)
+
+    # Ecto는 flush()위의 내용을 우선 실행하고 다음으로 진행을 넘어간다.
+    flush()
+
+    # 기존에 compositions에 등록된 artist_id들을 새로운 테이블인 compositions_artists로 옮긴다.
+    from(c in "compositions", select: [:id, :artist_id])
+    |> Repo.all()
+    |> Enum.each(fn row ->
+      Repo.insert_all("compositions_artists", [
+        [composition_id: row.id, artist_id: row.artist_id, role: "composer"]
+      ])
+    end)
+
+    # 더이상 필요가 없기때문에 지운다.
+    alter table("compositions") do
+      remove :artist_id
+    end
+
   end
 end
