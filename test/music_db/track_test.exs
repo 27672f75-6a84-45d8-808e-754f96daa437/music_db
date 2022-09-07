b593b20b76f4ff7a35dc966263518b1865631a06
twozeronine <amdusias209@gmail.com>
2022-09-07 17:19:11 +0900

    feat : 트랙 동시성 테스트

---
+++ test/music_db/track_test.exs
@@ -0,0 +1,45 @@
+defmodule MusicDB.TrackTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(MusicDB.Repo)
+  end
+
+  test "update track" do
+    play_function = fn ->
+      receive do
+        :continue -> :ok
+      end
+
+      "So What"
+      |> MusicDB.Track.by_title()
+      |> MusicDB.Repo.one!()
+      |> then(fn track ->
+        MusicDB.Track.changeset(track, %{number_of_plays: track.number_of_plays + 1})
+      end)
+      |> then(fn changeset ->
+        changeset |> MusicDB.Repo.update()
+      end)
+      |> then(fn {:ok, track} ->
+        IO.inspect(track.number_of_plays)
+      end)
+    end
+
+    tasks = [
+      Task.start(play_function),
+      Task.start(play_function),
+      Task.start(play_function),
+      Task.start(play_function),
+      Task.start(play_function)
+    ]
+
+    Enum.map(tasks, fn {:ok, task} ->
+      Ecto.Adapters.SQL.Sandbox.allow(MusicDB.Repo, self(), task)
+      send(task, :continue)
+    end)
+
+    Process.sleep(5_000)
+    # number_of_plays에서 1이 나옵니다.
+    assert MusicDB.Repo.get(MusicDB.Track, 1).number_of_plays == 5
+  end
+end
